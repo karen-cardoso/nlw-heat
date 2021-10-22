20664a278e3e91d439e107ef458fd22ccc82dd66
Karen Cardoso <kah.card@gmail.com>
2021-10-21 23:04:36 -0300

    implement function to get informations via api github

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const linksSocialMedia = {
   github: 'karen-cardoso',
   youtube: 'channel/UCvxKDqOGP4iEwL4HolrUhdg',
@@ -15,3 +16,20 @@ function changeSocialMediaLinks() {
 }
 
 changeSocialMediaLinks()
+
+function getGithubProfileInfos(){
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    userName.textContent = data.name
+    userBio.textContent = data.bio
+    userLink.href = data.html_url
+    userAvatar.src = data.avatar_url
+    userLogin.textContent = data.login
+    
+  })
+}
+
+getGithubProfileInfos()
